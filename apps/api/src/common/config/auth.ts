0e0064ed77e5261c12f8d0f3f8a87cb902aa636d
Davi Lucciola <davilucciola14@gmail.com>
2026-09-03 19:05:50 -0300

    fix: adicionando coluna issuer na tabela account

--- apps/api/src/common/config/auth.ts
+++ apps/api/src/common/config/auth.ts
@@ -13,6 +13,9 @@ export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: 'pg',
 	}),
+	account: {
+		identityStrategy: 'provider-id',
+	},
 	emailAndPassword: {
 		enabled: true,
 		minPasswordLength: 8,
