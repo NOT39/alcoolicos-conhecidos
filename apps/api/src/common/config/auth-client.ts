f75938623040075c26f2bfdf4b79df8ec5d44d1c
Not <not39.contato@gmail.com>
2026-09-03 18:09:31 -0300

    fix (api): resolve problema de tipo no authClient

--- apps/api/src/common/config/auth-client.ts
+++ apps/api/src/common/config/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from 'better-auth/client';
 import { emailOTPClient } from 'better-auth/client/plugins';
 
-export const authClient = createAuthClient({
+export const authClient: ReturnType<typeof createAuthClient> = createAuthClient({
 	plugins: [emailOTPClient()],
 });
